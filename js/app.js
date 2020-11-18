9dfd7b6a517691aa22814f8d708800aad26b9c5a
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-11-18 21:56:32 +0600

    about section complete

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// scrool reveal
+window.sr = ScrollReveal();
+sr.reveal("#about-img", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+});
+sr.reveal(".about-msg", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+});
+sr.reveal(".msg-main", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "300px",
+});
+sr.reveal(".msg-main-secondary", {
+  duration: 3000,
+  origin: "top",
+  distance: "300px",
+});
+sr.reveal(".msg-main-paragraph", {
+  duration: 3000,
+  origin: "center",
+  distance: "300px",
+});
+sr.reveal("#founder-name", {
+  duration: 3000,
+  distance: "400px",
+});
+sr.reveal("#designation", {
+  duration: 4000,
+  distance: "300px",
+});
