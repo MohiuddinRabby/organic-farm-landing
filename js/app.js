769ddbce794f88ca1fcd24d3fec019d9ffe31dd7
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-11-27 10:50:34 +0600

    client testomonial and gallery init done

--- js/app.js
+++ js/app.js
@@ -11,75 +11,82 @@ sr.reveal(".about-msg", {
   distance: "300px",
 });
 sr.reveal(".msg-main", {
-  duration: 3000,
+  duration: 2000,
   origin: "bottom",
   distance: "300px",
 });
 sr.reveal(".msg-main-secondary", {
-  duration: 3000,
+  duration: 2000,
   origin: "top",
   distance: "300px",
 });
 sr.reveal(".msg-main-paragraph", {
-  duration: 3000,
+  duration: 2000,
   origin: "center",
   distance: "300px",
 });
 sr.reveal("#founder-name", {
-  duration: 3000,
+  duration: 2000,
   distance: "400px",
 });
 sr.reveal("#designation", {
-  duration: 4000,
+  duration: 2000,
   distance: "300px",
 });
 sr.reveal("#recent-product-title", {
-  duration: 3000,
+  origin: "top",
+  duration: 2000,
+  distance: "300px",
 });
 sr.reveal(".product-card-top", {
-  duration: 2500,
+  duration: 2000,
   origin: "top",
   distance: "300px",
 });
 sr.reveal(".product-card-bottom", {
-  duration: 2500,
+  duration: 2000,
   origin: "bottom",
   distance: "300px",
 });
-$('.slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: true,
-  dots: false,
-  centerMode: true,
-  variableWidth: true,
-  infinite: true,
-  focusOnSelect: true,
-  cssEase: 'linear',
-  touchMove: true,
-  prevArrow:'<button class="slick-prev"> < </button>',
-  nextArrow:'<button class="slick-next"> > </button>',
-  
-  //         responsive: [                        
-  //             {
-  //               breakpoint: 576,
-  //               settings: {
-  //                 centerMode: false,
-  //                 variableWidth: false,
-  //               }
-  //             },
-  //         ]
+$('.testi2').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false
+    },
+    1170: {
+      items: 1
+    }
+  }
 });
 
+$(function() {
+    // 1) ASSIGN EACH 'DOT' A NUMBER
+    dotcount = 1;
+    $('.testi2 .owl-dot').each(function() {
+        $(this).addClass('dotnumber' + dotcount);
+        $(this).attr('data-info', dotcount);
+        dotcount = dotcount + 1;
+    });
+    // 2) ASSIGN EACH 'SLIDE' A NUMBER
+    slidecount = 1;
+    $('.testi2 .owl-item').not('.cloned').each(function() {
+        $(this).addClass('slidenumber' + slidecount);
+        slidecount = slidecount + 1;
+    });
+    $('.testi2 .owl-dot').each(function() {
+        grab = jQuery(this).data('info');
+        slidegrab = $('.slidenumber' + grab + ' img').attr('src');
+        console.log(slidegrab);
+        $(this).css("background-image", "url(" + slidegrab + ")");
+    });
+    // THIS FINAL BIT CAN BE REMOVED AND OVERRIDEN WITH YOUR OWN CSS OR FUNCTION, I JUST HAVE IT
+    // TO MAKE IT ALL NEAT 
 
-var imgs = $('.slider img');
-imgs.each(function(){
-  var item = $(this).closest('.item');
-  item.css({
-    'background-image': 'url(' + $(this).attr('src') + ')', 
-    'background-position': 'center',            
-    '-webkit-background-size': 'cover',
-    'background-size': 'cover', 
-  });
-  $(this).hide();
 });
